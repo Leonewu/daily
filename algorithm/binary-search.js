f3caa06c224edb1b9dd9d355b51b62970bfcdf46
leonewu <379747139@qq.com>
2019-12-12 19:27:40 +0800

    binary search的两种写法

---
+++ algorithm/binary-search.js
@@ -0,0 +1,30 @@
+
+// 在有序数组中找到下标，没有找到返回 -1
+const recursion_binary_search = (arr, target, start, end) => {
+  start = start || 0
+  end = end || arr.length - 1
+  const mid = parseInt((start + end) / 2)
+  if (arr[mid] === target) {
+    return mid
+  } else if (arr[mid] < target) {
+    return recursion_binary_search(arr, target, mid + 1, end)
+  } else {
+    return recursion_binary_search(arr, target, start, mid - 1)
+  }
+}
+
+const while_binary_search = (arr, target) => {
+  let start = 0, end = arr.length - 1
+  while (start <= end) {
+    const mid = parseInt((start + end) / 2)
+    if (arr[mid] === target) {
+      return mid
+    } else if (arr[mid] < target) {
+      start = mid + 1
+    } else {
+      end = mid - 1
+    }
+  }
+  return -1
+}
+
