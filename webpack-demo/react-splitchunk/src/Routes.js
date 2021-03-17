4ea21677263ae45ba3bd9bfc1c6bfea40d773e53
吴高亮 <379747139@qq.com>
2021-03-17 15:31:29 +0800

    :package: webpack analyze bundler and dynamic import route

--- webpack-demo/react-splitchunk/src/Routes.js
+++ webpack-demo/react-splitchunk/src/Routes.js
@@ -3,22 +3,23 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
-import Page1 from './page/Page1';
-import Page2 from './page/Page2';
-import Page3 from './page/Page3';
 import Home from './page/Home';
+// import Page1 from './page/Page1';
+// import Page2 from './page/Page2';
+// import Page3 from './page/Page3';
+import lodabale from '@loadable/component';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/page1">
-        <Page1 />
+      <Route path="/page1" component={lodabale(() => import('./page/Page1'))}>
+        {/* <Page1 /> */}
       </Route>
-      <Route path="/page2">
-        <Page2 />
+      <Route path="/page2" component={lodabale(() => import('./page/Page2'))}>
+        {/* <Page2 /> */}
       </Route>
-      <Route path="/page3">
-        <Page3 />
+      <Route path="/page3" component={lodabale(() => import('./page/Page3'))}>
+        {/* <Page3 /> */}
       </Route>
       <Route path="/">
         <Home />
