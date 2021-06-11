7145f1d9b12db527845f1809b2351ff30484ab2f
吴高亮 <379747139@qq.com>
2021-06-11 14:17:37 +0800

    :bug: optimize: binary search

--- algorithm/binary-search.js
+++ algorithm/binary-search.js
@@ -3,7 +3,7 @@
 const recursion_binary_search = (arr, target, start, end) => {
   start = start || 0
   end = typeof end === 'number' ? end : arr.length - 1
-  const mid = parseInt((start + end) / 2)
+  const mid = Math.floor((start + end) / 2)
   if (start > end) return -1
   if (arr[mid] === target) {
     return mid
@@ -17,7 +17,7 @@ const recursion_binary_search = (arr, target, start, end) => {
 const while_binary_search = (arr, target) => {
   let start = 0, end = arr.length - 1
   while (start <= end) {
-    const mid = parseInt((start + end) / 2)
+    const mid = Math.floor((start + end) / 2)
     if (arr[mid] === target) {
       return mid
     } else if (arr[mid] < target) {
@@ -49,3 +49,68 @@ function binarySearch(arr, target, offset = 0) {
   }
   return -1;
 }
+
+// 二分查找应用
+// 在有序数组中寻找某个数字出现的次数
+// https://leetcode-cn.com/problems/zai-pai-xu-shu-zu-zhong-cha-zhao-shu-zi-lcof/
+// search([5,7,7,8,8,10], 6) => 0
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var search = function(nums, target) {
+  if (nums.length <= 1) {
+     return (nums.length === 1 && nums[0] === target) ? 1 : 0;
+   }
+   let times = 0;
+   const index = Math.floor(nums.length / 2);
+   if (nums[index] === target) {
+     times++;
+     let leftIndex = index;
+     let rightIndex = index;
+     let leftEnd = false;
+     let rightEnd = false;
+     while (!leftEnd || !rightEnd) {
+       if (leftIndex === 0) {
+         leftEnd = true;
+       }
+       leftIndex--;
+       if (target === nums[leftIndex]) {
+         times++;
+       } else {
+         leftEnd = true;
+       }
+       if (rightIndex === nums.length - 1) {
+         rightEnd = true;
+       }
+       rightIndex++;
+       if (target === nums[rightIndex]) {
+         times++;
+       } else {
+         rightEnd = true;
+       }
+     }
+   }
+   if (nums[index] > target) {
+     times += search(nums.slice(0, index), target);
+   }
+   if (nums[index] < target) {
+     times += search(nums.slice(index, nums.length), target);
+   }
+   return times;
+ };
+
+
+
+// 二分查找的复杂度分析
+// 时间复杂度
+// 最坏情况下
+// 最优情况下
+// 平均
+
+
+// 空间复杂度
+// 最坏情况下
+// 最优情况下
+// 平均
